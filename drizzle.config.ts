498aa5c8a40e15293063ff9a7643a14ae0feb860
AnkitNayan13 <ankit.nayan@mathongo.com>
2025-02-18 14:39:27 +0530

    INIT

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["ankits_portfolio_*"],
+} satisfies Config;
