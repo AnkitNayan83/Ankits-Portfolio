0392083a76214f70e86c6d07ca647971c28d29c5
AnkitNayan83 <ankitnayan83@gmail.com>
2025-03-22 13:05:58 +0530

    fixed params

--- src/app/(pages)/blogs/(admin)/update/[id]/page.tsx
+++ src/app/(pages)/blogs/(admin)/update/[id]/page.tsx
@@ -2,13 +2,11 @@ import { CreateBlogForm } from "@/app/_components/create-blog-form";
 import { api } from "@/trpc/server";
 
 interface UpdatePageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 const UpdatePage = async ({ params }: UpdatePageProps) => {
-  const blogId = parseInt(params.id);
+  const blogId = parseInt((await params).id);
   const blog = await api.blog.getBlog({ id: blogId });
 
   if (!blog) return <span>Blog not found</span>;
